3d820a6c850e7acbf403c7572ca023aef60e9c0d
Gor <7495500@gmail.com>
2024-10-13 18:28:48 +0300

    Remove some views, modify posts and comments controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,42 +1,14 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
-
-  def index
-    @comments = Comment.all
-  end
-
-  def show
-  end
-
-  def new
-    @comment = Comment.new(parent_id: params[:parent_id])
-  end
-
-  def edit
-  end
 
   def create
     @comment = Comment.new(comment_params)
 
     if @comment.save
-      redirect_to @comment.post, notice: "Comment was successfully created"
+      redirect_to @comment.post
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
-  end
-
-  def update
-    if @comment.update(comment_params)
-      redirect_to @comment, notice: "Comment was successfully updated"
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @comment.destroy
-    redirect_to comments_path, status: :see_other, notice: "Comment was successfully destroyed"
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,42 +1,25 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :set_post, only: %i[ show ]
   before_action :authenticate_user!
 
   def index
     @posts = Post.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @post = Post.new
-  end
-
-  def edit
   end
 
   def create
     @post = Post.new(post_params)
 
     if @post.save
-      redirect_to post_path(@post), notice: "Post was successfully created"
+      redirect_to post_path(@post)
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
-  end
-
-  def update
-    if @post.update(post_params)
-      redirect_to @post, notice: "Post was successfully updated"
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @post.destroy
-    redirect_to posts_path, status: :see_other, notice: "Post was successfully destroyed"
   end
 
   private
@@ -46,6 +29,10 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :body, :user_id)
+    params.require(:post).permit(
+      :title,
+      :body,
+      :user_id
+    )
   end
 end
